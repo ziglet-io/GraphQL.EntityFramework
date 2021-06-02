b2f475407035d877a3419077f492514fd4119958
Jeffrey Hohenstein <jeff@ziglet.io>
2021-06-01 21:51:08 -0700

    Modify signature in IFilter

--- src/GraphQL.EntityFramework/Filters/IFilter.cs
+++ src/GraphQL.EntityFramework/Filters/IFilter.cs
@@ -11,9 +11,21 @@ namespace GraphQL.EntityFramework
     /// </summary>
     public interface IFilter<TEntity> where TEntity : class
     {
-        public delegate bool Filter(object userContext, TEntity input);
+        /// <summary>
+        /// Return true if the system should return the entity. False if not.
+        /// </summary>
+        /// <param name="userContext">User context from the caller. For example in an ASP.NET application should be <see cref="UserContext"/> or similar.</param>
+        /// <param name="input">The <see cref="TEntity"/> that will be output if the filter succeeds.</param>
+        /// <returns></returns>
+        public bool Filter(object userContext, TEntity input);
 
-        public delegate Task<bool> AsyncFilter(object userContext, TEntity input);
+        /// <summary>
+        /// Return true if the system should return the entity. False if not.
+        /// </summary>
+        /// <param name="userContext">User context from the caller. For example in an ASP.NET application should be <see cref="UserContext"/> or similar.</param>
+        /// <param name="input">The <see cref="TEntity"/> that will be output if the filter succeeds.</param>
+        /// <returns></returns>
+        public Task<bool> AsyncFilter(object userContext, TEntity input);
 
     }
 }
